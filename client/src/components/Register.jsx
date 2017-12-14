11126c1982b68e93d85d3484ed4527195cea3d75
Olivia Stoick <oliviastoick@gmail.com>
2017-12-14 12:33:01 -0500

    completed register component

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+class Register extends Component {
+  constructor () {
+    super()
+    this.state = {
+      username: '',
+      password: '',
+      email: ''
+    }
+    this.handleInputChange = this.handlenputChange.bind(this)
+  }
+
+  handleInputChange (e) {
+    const name = e.target.username
+    const value = e.target.value
+    this.setState({
+      [name]: value
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <form onSubmit={(e) => this.props.handleRegisterSubmit(e, this.state)}>
+          <input type='text' name='username' value={this.state.username} placeholder='Username' onChange={this.handleInputChange} />
+          <input type='password' name='password' value={this.state.password} placeholder='Password' onChange={this.handleInputChange} />
+          <input type='email' name='email' value={this.state.email} placeholder='email' onChange={this.handleInputChange} />
+          <input type='submit'>Register</input>
+        </form>
+      </div>
+    )
+  }
+}
+export default Register
