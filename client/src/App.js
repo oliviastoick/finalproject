6d407f8019b474a9e6abcf2febb50a1690b89670
Olivia Stoick <oliviastoick@gmail.com>
2017-12-14 10:32:26 -0500

    set up client and Header and app components

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      auth: false,
+      user: null
+    }
+  }
+
+  render () {
+    return (
+      <Router>
+        <div className='App'>
+          <Header />
+          <div className='container'>
+            <Route exact path='/' component={Home} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App
