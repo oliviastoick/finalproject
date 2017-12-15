a3f0e636d1a74440eaf7b930b5646da768e6a05d
Olivia Stoick <oliviastoick@gmail.com>
2017-12-15 10:54:37 -0500

    created a content controller

---
+++ controllers/content-controller.js
@@ -0,0 +1,59 @@
+const Content = require('../models/Content')
+const contentController = {}
+
+contentController.index = (req, res, next) => {
+  Content.findAll()
+    .then(content => {
+      res.json({
+        message: 'ok',
+        data: { content }
+      })
+    }).catch(next)
+}
+
+contentController.show = (req, res, next) => {
+  Content.findById(req.params.id)
+    .then(content => {
+      res.json({
+        message: 'ok',
+        data: { content }
+      })
+    }).catch(next)
+}
+
+contentController.create = (req, res, next) => {
+  Content.create({
+    title: req.body.title,
+    description: req.body.description,
+    genre: req.body.genre
+  }, req.user.id).then(content => {
+    res.json({
+      message: 'Post added successfully!',
+      data: { content }
+    })
+  }).catch(next)
+}
+
+contentController.update = (req, res, next) => {
+  Content.update({
+    title: req.body.title,
+    description: req.body.description,
+    genre: req.body.genre
+  }, req.params.id).then(movie => {
+    res.json({
+      message: 'Post updated successfully!',
+      data: { movie }
+    })
+  }).catch(next)
+}
+
+contentController.delete = (req, res, next) => {
+  Content.destroy(req.params.id)
+    .then(() => {
+      res.json({
+        message: 'Post deleted successfully!'
+      })
+    }).catch(next)
+}
+
+module.exports = contentController
